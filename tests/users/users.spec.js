b52737b0272a393397b58da64069ddd22e9fbd0b
Noah <noahkimtai24@gmail.com>
2023-08-22 12:13:17 +0300

    use faker to generate random data every time the script is ran

--- tests/users/users.spec.js
+++ tests/users/users.spec.js
@@ -1,4 +1,5 @@
 const {test, expect } = require('@playwright/test');
+import { faker } from '@faker-js/faker';
 
 // // Getting all the system users test
 // test('Get all users', async({request})=> {
@@ -28,34 +29,34 @@ const {test, expect } = require('@playwright/test');
 //     console.log(responseData)
 // })
 
-// Test assigning a role to a user
-test('Assign a role to a user', async ({ request }) => {
-    const response = await request.post('/users/1031/switch/1', {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json', // Corrected typo here
-        },
-        data: {
-            "assignedSystems": [38102297, 20938489],
-        },
-    });
+// // Test assigning a role to a user
+// test('Assign a role to a user', async ({ request }) => {
+//     const response = await request.post('/users/1031/switch/1', {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json', // Corrected typo here
+//         },
+//         data: {
+//             "assignedSystems": [38102297, 20938489],
+//         },
+//     });
 
-    // Assuming you expect the response to be JSON, use try-catch to handle potential parsing errors
-    try {
-        let responseData = await response.json();
-        console.log(responseData);
-    } catch (error) {
-        console.error('JSON parsing error:', error);
-    }
+//     // Assuming you expect the response to be JSON, use try-catch to handle potential parsing errors
+//     try {
+//         let responseData = await response.json();
+//         console.log(responseData);
+//     } catch (error) {
+//         console.error('JSON parsing error:', error);
+//     }
 
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
-});
+//     expect(response.ok()).toBeTruthy();
+//     expect(response.status()).toBe(200);
+// });
 
 // const data = {
-//     "name": "QA name",
-//     "username": "QA uname",
-//     "emailAddress": "qamgo@gmail.com",
+//     "name": faker.person.fullName(),
+//     "username": faker.internet.userName(),
+//     "emailAddress": faker.internet.email(),
 //     "dateOfBirth": "0025-11-30T00:00:00.000+00:00",
 //     "status": "A",
 //     "userType": "U",
@@ -77,8 +78,8 @@ test('Assign a role to a user', async ({ request }) => {
 //     "supervisorId": 16682847,
 //     "branchId": null,
 //     "gender": "M",
-//     "pinNumber": "A859210198X",
-//     "idNumber": "34181100"
+//     "pinNumber": `A${faker.string.alphanumeric(9)}X`,
+//     "idNumber": faker.string.alphanumeric(8),
 //     }
 // // Test a post request for users end point
 // let createdUserId;
@@ -114,12 +115,16 @@ test('Assign a role to a user', async ({ request }) => {
 //     },
 //     )
 // })
-// test('Delete the created request', async({request}) =>{
-//     const response = await request.delete('/users/16683207',//`/users/${createdUserId}`,
-//     { 'Content-Type': 'application/json'});
-//     let responseData = await response.json()
-//     console.log(responseData)
-//     expect (response.ok()).toBeTruthy();
-//     expect (response.status()).toBe(200)
+test('Delete the created request', async({request}) =>{
+    const response = await request.delete('/users/16683207',//`/users/${createdUserId}`,
+    { 'Content-Type': 'application/json'});
+    // let responseData = await response.json()
+    // console.log(responseData)
+    expect (response.ok()).toBeTruthy();
+    expect (response.status()).toBe(200)
+    //check data is deleted by sending a get request.
+    const newResponse = await request.get('/users/16682947')
+    expect (newResponse.ok()).toBeFalsy();
+    expect (newResponse.status()).toBe("BAD_REQUEST")
 
-// })
+})
